9ddd0bc0c7ada08f60ad59af2c54ed1765cce27b
Martin Skow Røed <msroed@gmail.com>
2014-05-19 13:56:29 +0200

    added stickies

--- kybsal/timer/models.py
+++ kybsal/timer/models.py
@@ -82,6 +82,7 @@ class FeedItem(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	slave = models.ForeignKey('slave.Slave', related_name='feed')
 	desc = models.CharField(max_length=100)
+	sticky = models.BooleanField(default=False)
 
 	def __unicode__(self):
 		return self.desc
